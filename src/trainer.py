32716257f975c7c58c19a9e78b066fc78fc5161c
m333il <alexandrovms333@gmail.com>
2025-11-09 23:47:38 +0300

    refactor trainer

--- src/trainer.py
+++ src/trainer.py
@@ -37,23 +37,58 @@ class Trainer:
         logging.info(f'Trainer.__init__: finish')
 
 
+    def _prepare_batch(self, batch, device):
+        return {k: v.to(device) for k, v in batch.items()}
+    
+    
+    def _training_step(self, batch):
+        self.optimizer.zero_grad()
+        outputs = self.model(**batch)
+        loss = outputs.loss
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+    
+    
+    def _compute_epoch_loss(self, losses):
+        if len(losses) == 0:
+            return 0.0
+        return sum(losses) / len(losses)
+    
+    
     def _one_epoch(self, epoch_num):
         logging.info(f'Trainer._one_epoch: start')
         self.model.train()
-        total_loss = 0
+        losses = []
         
         for batch in tqdm(self.train_dl, desc=f"Training: epoch {epoch_num}", leave=False):
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            self.optimizer.zero_grad()
-            loss = self.model(**batch).loss
-            loss.backward()
-            self.optimizer.step()
-            total_loss += loss.item()
-        total_loss /= len(self.train_dl)
+            prepared_batch = self._prepare_batch(batch, self.device)
+            loss = self._training_step(prepared_batch)
+            losses.append(loss)
+        
+        avg_loss = self._compute_epoch_loss(losses)
         logging.info(f'Trainer._one_epoch: finish')
-        return total_loss
+        return avg_loss
 
 
+    def _prepare_batch_for_inference(self, batch, device):
+        labels = batch.pop("labels")
+        prepared_batch = {k: v.to(device) for k, v in batch.items()}
+        return prepared_batch, labels
+    
+    
+    def _inference_step(self, batch):
+        outputs = self.model(**batch)
+        predictions = outputs.logits.cpu().argmax(dim=-1)
+        return predictions
+    
+    
+    def _aggregate_predictions(self, all_predictions, all_labels):
+        y_pred = torch.cat(all_predictions)
+        y_true = torch.cat(all_labels)
+        return y_true.numpy(), y_pred.numpy()
+    
+    
     @torch.no_grad()
     def _predict(self):
         logging.info(f'Trainer._predict: start')
@@ -62,16 +97,14 @@ class Trainer:
         y_true = []
         
         for batch in tqdm(self.val_dl, desc="Predicting"):
-            labels = batch.pop("labels")
-            batch = {k: v.to(self.device) for k, v in batch.items()}
-            outputs = self.model(**batch)
-            y_pred.append(outputs.logits.cpu().argmax(dim=-1))
+            prepared_batch, labels = self._prepare_batch_for_inference(batch, self.device)
+            predictions = self._inference_step(prepared_batch)
+            y_pred.append(predictions)
             y_true.append(labels.cpu())
         
-        y_pred = torch.cat(y_pred)
-        y_true = torch.cat(y_true)
+        true_labels, pred_labels = self._aggregate_predictions(y_pred, y_true)
         logging.info(f'Trainer._predict: finish')
-        return y_true.numpy(), y_pred.numpy(),
+        return true_labels, pred_labels
 
 
     def evaluate(self):
